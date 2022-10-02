05eae01562f91796881f500210f480011c949d92
Egito Filho <egitofilho4@gmail.com>
2022-10-02 18:35:28 -0300

    shared preference impl n removed mutex

--- app/src/main/java/com/picpay/desafio/android/data/repository/PicPayRepositoryImpl.kt
+++ app/src/main/java/com/picpay/desafio/android/data/repository/PicPayRepositoryImpl.kt
@@ -1,31 +1,13 @@
 package com.picpay.desafio.android.data.repository
 
 import com.picpay.desafio.android.data.datasource.PicPayDataSource
-import com.picpay.desafio.android.data.response.UserResponse
 import com.picpay.desafio.android.ui.User
 import com.picpay.desafio.android.utils.toUser
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import javax.inject.Inject
 
 class PicPayRepositoryImpl @Inject constructor(
     private val dataSource: PicPayDataSource
 ) : PicPayRepository {
-
-    private val latestUsersMutex = Mutex()
-
-    private var latestUsers: List<User> = emptyList()
-
-    suspend fun getLatestUsers(refresh: Boolean = false): List<User> {
-        if (refresh || latestUsers.isEmpty()) {
-            val networkResult = dataSource.getUsers().map { userResponse -> userResponse.toUser()}
-            latestUsersMutex.withLock {
-                this.latestUsers = networkResult
-            }
-        }
-
-        return latestUsersMutex.withLock { this.latestUsers }
-    }
 
     override suspend fun getUsers(): List<User> {
         return dataSource.getUsers()
--- app/src/main/java/com/picpay/desafio/android/data/response/UserResponse.kt
+++ app/src/main/java/com/picpay/desafio/android/data/response/UserResponse.kt
@@ -1,8 +1,5 @@
 package com.picpay.desafio.android.data.response
 
-import kotlinx.serialization.Serializable
-
-@Serializable
 data class UserResponse(
     val id: String,
     val name: String,
