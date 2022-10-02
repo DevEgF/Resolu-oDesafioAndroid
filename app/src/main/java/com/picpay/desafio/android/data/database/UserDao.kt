6ce7d846e0a4e88daee1fcec2220bc3731d85f8d
Egito Filho <104474051+DevEgF@users.noreply.github.com>
2022-10-02 17:17:30 -0300

    room implementation

---
+++ app/src/main/java/com/picpay/desafio/android/data/database/UserDao.kt
@@ -0,0 +1,13 @@
+package com.picpay.desafio.android.data.database
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.picpay.desafio.android.ui.User
+
+@Dao
+interface UserDao {
+//
+//    @Query("SELECT * FROM user_entity)
+//            fun getAllRecordsFromDB(): List<UserEntity>?
+
+}
