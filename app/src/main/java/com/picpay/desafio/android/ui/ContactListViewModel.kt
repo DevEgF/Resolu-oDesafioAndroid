c0269491a92b619c5f79bc6deb8fec374a2aceee
Egito Filho <104474051+DevEgF@users.noreply.github.com>
2022-10-03 09:12:54 -0300

    viewmodel room

--- app/src/main/java/com/picpay/desafio/android/ui/ContactListViewModel.kt
+++ app/src/main/java/com/picpay/desafio/android/ui/ContactListViewModel.kt
@@ -1,5 +1,7 @@
 package com.picpay.desafio.android.ui
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,6 +10,7 @@ import com.picpay.desafio.android.data.database.Entity
 import com.picpay.desafio.android.data.database.RoomDataBase
 import com.picpay.desafio.android.data.database.UsersRepository
 import com.picpay.desafio.android.data.repository.PicPayRepository
+import dagger.hilt.android.internal.Contexts.getApplication
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -15,11 +18,13 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ContactListViewModel @Inject constructor(
+    application: Application,
     private val picPayRepository: PicPayRepository
-) : ViewModel() {
+) : AndroidViewModel(application) {
 
     private val repository: UsersRepository
     private val readAll: List<Entity>
+    private val context = getApplication<Application>().applicationContext
 
 
     private val _uiState = MutableLiveData<List<User>>()
@@ -27,7 +32,7 @@ class ContactListViewModel @Inject constructor(
 
     init {
         fetchUsers()
-        val noteDB = RoomDataBase.getDatabase().userDao()
+        val noteDB = RoomDataBase.getDatabase(context).userDao()
         repository = UsersRepository(noteDB)
         readAll = repository.getAllUsers()
     }
